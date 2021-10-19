045626e9fe83364d11421f8899f380a9c4364fc3
marcus <marcmonk@gmail.com>
2021-10-19 18:02:34 -0400

    update

--- current.js
+++ current.js
@@ -9,12 +9,20 @@ Check the assertion tests for examples.
 */
 
 function uniteUnique(arr) {
-  return arr;
+  let args = [...arguments];
+  let joined = args.reduce((accumulator, arg) => {
+    return [...accumulator, ...arg];
+    //accumulator = [...arg]
+    //accumulator.concat(arg);    
+  });
+  return joined;
 }
 
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]), JSON.stringify(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])) === JSON.stringify([1, 3, 2, 5, 4]));
-console.log(uniteUnique([1, 2, 3], [5, 2, 1]), JSON.stringify(uniteUnique([1, 2, 3], [5, 2, 1])) === JSON.stringify([1, 2, 3, 5]));
-console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]), JSON.stringify(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])) === JSON.stringify([1, 2, 3, 5, 4, 6, 7, 8]));
+//console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+ console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]), JSON.stringify(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])) === JSON.stringify([1, 3, 2, 5, 4]));
+ console.log(uniteUnique([1, 2, 3], [5, 2, 1]), JSON.stringify(uniteUnique([1, 2, 3], [5, 2, 1])) === JSON.stringify([1, 2, 3, 5]));
+ console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]), JSON.stringify(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])) === JSON.stringify([1, 2, 3, 5, 4, 6, 7, 8]));
 
 /*
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
