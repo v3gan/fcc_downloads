4ff0c14e6617ed84a2313503f603bc19b57f824d
marcus <marcmonk@gmail.com>
2021-11-08 14:50:37 -0500

    set up telephone number challenge

--- curr.js
+++ curr.js
@@ -1,41 +1,133 @@
 /*
 
-One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+Return true if the passed string looks like a valid US phone number.
 
-A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
 
-Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
 
-All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
 */
 
-function rot13(str) {
-  let alphabet = {};
-  let factor = 0;
-  for (let i = 65; i < 91; i++) {
-    let code = (i + 13) > 90 ? i - 13 : i + 13;
-    alphabet[String.fromCharCode(code)] = String.fromCharCode(i);
-  }
-  let decoded = str.split('').map(item => /\w/.test(item) ? alphabet[item] : item).join('');
-  return decoded;
+function telephoneCheck(str) {
+  return true;
 }
 
-let x = rot13("SERR PBQR PNZC");
-console.log(x, x === "FREE CODE CAMP");
-x = rot13("SERR CVMMN!");
-console.log(x, x === "FREE PIZZA!");
-x = rot13("SERR YBIR?");
-console.log(x, x === "FREE LOVE?");
-x = rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
-console.log(x, x === "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.");
+let x = telephoneCheck("1 555-555-5555");
+console.log(x, x === true);
+x = telephoneCheck("1 (555) 555-5555");
+console.log(x, x === true);
+x = telephoneCheck("5555555555");
+console.log(x, x === true);
+x = telephoneCheck("555-555-5555");
+console.log(x, x === true);
+x = telephoneCheck("(555)555-5555");
+console.log(x, x === true);
+x = telephoneCheck("1(555)555-5555");
+console.log(x, x === true);
+x = telephoneCheck("555-5555");
+console.log(x, x === false);
+x = telephoneCheck("5555555");
+console.log(x, x === false);
+x = telephoneCheck("1 555)555-5555");
+console.log(x, x === false);
+x = telephoneCheck("1 555 555 5555");
+console.log(x, x === true);
+x = telephoneCheck("1 456 789 4444");
+console.log(x, x === true);
+x = telephoneCheck("123**&!!asdf#");
+console.log(x, x === false);
+x = telephoneCheck("55555555");
+console.log(x, x === false);
+x = telephoneCheck("(6054756961)");
+console.log(x, x === false);
+x = telephoneCheck("2 (757) 622-7382");
+console.log(x, x === false);
+x = telephoneCheck("0 (757) 622-7382");
+console.log(x, x === false);
+x = telephoneCheck("-1 (757) 622-7382");
+console.log(x, x === false);
+x = telephoneCheck("2 757 622-7382");
+console.log(x, x === false);
+x = telephoneCheck("10 (757) 622-7382");
+console.log(x, x === false);
+x = telephoneCheck("27576227382");
+console.log(x, x === false);
+x = telephoneCheck("(275)76227382");
+console.log(x, x === false);
+x = telephoneCheck("2(757)6227382");
+console.log(x, x === false);
+x = telephoneCheck("2(757)622-7382");
+console.log(x, x === false);
+x = telephoneCheck("555)-555-5555");
+console.log(x, x === false);
+x = telephoneCheck("(555-555-5555");
+console.log(x, x === false);
+x = telephoneCheck("(555)5(55?)-5555");
+console.log(x, x === false);
+x = telephoneCheck("55 55-55-555-5");
+console.log(x, x === false);
 
 /*
-rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+telephoneCheck("555-555-5555") should return a boolean.
 
-rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+telephoneCheck("1 555-555-5555") should return true.
 
-rot13("SERR YBIR?") should decode to the string FREE LOVE?
+telephoneCheck("1 (555) 555-5555") should return true.
 
-rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+telephoneCheck("5555555555") should return true.
+
+telephoneCheck("555-555-5555") should return true.
+
+telephoneCheck("(555)555-5555") should return true.
+
+telephoneCheck("1(555)555-5555") should return true.
+
+telephoneCheck("555-5555") should return false.
+
+telephoneCheck("5555555") should return false.
+
+telephoneCheck("1 555)555-5555") should return false.
+
+telephoneCheck("1 555 555 5555") should return true.
+
+telephoneCheck("1 456 789 4444") should return true.
+
+telephoneCheck("123**&!!asdf#") should return false.
+
+telephoneCheck("55555555") should return false.
+
+telephoneCheck("(6054756961)") should return false.
+
+telephoneCheck("2 (757) 622-7382") should return false.
+
+telephoneCheck("0 (757) 622-7382") should return false.
+
+telephoneCheck("-1 (757) 622-7382") should return false.
+
+telephoneCheck("2 757 622-7382") should return false.
+
+telephoneCheck("10 (757) 622-7382") should return false.
+
+telephoneCheck("27576227382") should return false.
+
+telephoneCheck("(275)76227382") should return false.
+
+telephoneCheck("2(757)6227382") should return false.
+
+telephoneCheck("2(757)622-7382") should return false.
+
+telephoneCheck("555)-555-5555") should return false.
+
+telephoneCheck("(555-555-5555") should return false.
+
+telephoneCheck("(555)5(55?)-5555") should return false.
+
+telephoneCheck("55 55-55-555-5") should return false.
 */
