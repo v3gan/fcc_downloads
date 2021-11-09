506eb6dadc6f42991b8f605fff180b3c1b2bfc5d
marcus <marcmonk@gmail.com>
2021-11-09 17:48:52 -0500

    working on cash register reduce function

--- curr.js
+++ curr.js
@@ -1,133 +1,112 @@
 /*
 
-Return true if the passed string looks like a valid US phone number.
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
-The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+cid is a 2D array listing available currency.
 
-555-555-5555
-(555)555-5555
-(555) 555-5555
-555 555 5555
-5555555555
-1 555 555 5555
+The checkCashRegister() function should always return an object with a status key and a change key.
 
-For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+See below for an example of a cash-in-drawer array:
+
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]
 
 */
 
-function telephoneCheck(str) {
-  return true;
+function CashRegister(drawer) {
+  this.cid = drawer;
+
+  this.doTransaction = function(sale, tendered) {
+    let totalInDrawer = this.cid.reduce((prev, curr) => {    
+      return prev + curr[1];
+    }, 0);
+    if (totalInDrawer - sale > 0) {
+      return {status: "OPEN", change: this.makeChange(tendered - sale)};
+    } else {
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+  }
+
+  this.makeChange = function(amount){
+    const denomValue = {
+      'PENNY': .01,
+      'NICKEL': .05,
+      'DIME': .1,
+      'QUARTER': .25,
+      'ONE': 1,
+      'FIVE': 5,
+      'TEN': 10,
+      'TWENTY': 20,
+      'ONE HUNDRED': 100
+    }
+
+    let denomCount = this.cid
+      .map(val => [...val, Math.round(val[1]/denomValue[val[0]]), Math.floor(amount / denomValue[val[0]]) ]).reverse();
+
+    let change = denomCount.reduce((prev, current) => {
+      
+    }, amount);
+
+    console.log(denomCount);
+
+    return [['QUARTER', amount/denomValue.NICKEL]];
+  }
 }
 
-let x = telephoneCheck("1 555-555-5555");
-console.log(x, x === true);
-x = telephoneCheck("1 (555) 555-5555");
-console.log(x, x === true);
-x = telephoneCheck("5555555555");
-console.log(x, x === true);
-x = telephoneCheck("555-555-5555");
-console.log(x, x === true);
-x = telephoneCheck("(555)555-5555");
-console.log(x, x === true);
-x = telephoneCheck("1(555)555-5555");
-console.log(x, x === true);
-x = telephoneCheck("555-5555");
-console.log(x, x === false);
-x = telephoneCheck("5555555");
-console.log(x, x === false);
-x = telephoneCheck("1 555)555-5555");
-console.log(x, x === false);
-x = telephoneCheck("1 555 555 5555");
-console.log(x, x === true);
-x = telephoneCheck("1 456 789 4444");
-console.log(x, x === true);
-x = telephoneCheck("123**&!!asdf#");
-console.log(x, x === false);
-x = telephoneCheck("55555555");
-console.log(x, x === false);
-x = telephoneCheck("(6054756961)");
-console.log(x, x === false);
-x = telephoneCheck("2 (757) 622-7382");
-console.log(x, x === false);
-x = telephoneCheck("0 (757) 622-7382");
-console.log(x, x === false);
-x = telephoneCheck("-1 (757) 622-7382");
-console.log(x, x === false);
-x = telephoneCheck("2 757 622-7382");
-console.log(x, x === false);
-x = telephoneCheck("10 (757) 622-7382");
-console.log(x, x === false);
-x = telephoneCheck("27576227382");
-console.log(x, x === false);
-x = telephoneCheck("(275)76227382");
-console.log(x, x === false);
-x = telephoneCheck("2(757)6227382");
-console.log(x, x === false);
-x = telephoneCheck("2(757)622-7382");
-console.log(x, x === false);
-x = telephoneCheck("555)-555-5555");
-console.log(x, x === false);
-x = telephoneCheck("(555-555-5555");
-console.log(x, x === false);
-x = telephoneCheck("(555)5(55?)-5555");
-console.log(x, x === false);
-x = telephoneCheck("55 55-55-555-5");
-console.log(x, x === false);
+function checkCashRegister(price, cash, cid) {
+  //console.log(cid[0]);
+  const register = new CashRegister(cid);
+  let x = register.doTransaction(price, cash);
+  return x; //register.doTransaction(price, cash);
+}
+
+let x = checkCashRegister(19.25, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(x, JSON.stringify(x) === JSON.stringify({status: "OPEN", change: [["QUARTER", 0.5]]}));
+// x = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+// console.log(x, JSON.stringify(x) === JSON.stringify({status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}));
+// x = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+// console.log(x, JSON.stringify(x) === JSON.stringify({status: "INSUFFICIENT_FUNDS", change: []}));
+// x = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+// console.log(x, JSON.stringify(x) === JSON.stringify({status: "INSUFFICIENT_FUNDS", change: []}));
+// x = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+// console.log(x, JSON.stringify(x) === JSON.stringify({status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}));
+
 
 /*
-telephoneCheck("555-555-5555") should return a boolean.
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
 
-telephoneCheck("1 555-555-5555") should return true.
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
 
-telephoneCheck("1 (555) 555-5555") should return true.
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 
-telephoneCheck("5555555555") should return true.
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
 
-telephoneCheck("555-555-5555") should return true.
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
 
-telephoneCheck("(555)555-5555") should return true.
-
-telephoneCheck("1(555)555-5555") should return true.
-
-telephoneCheck("555-5555") should return false.
-
-telephoneCheck("5555555") should return false.
-
-telephoneCheck("1 555)555-5555") should return false.
-
-telephoneCheck("1 555 555 5555") should return true.
-
-telephoneCheck("1 456 789 4444") should return true.
-
-telephoneCheck("123**&!!asdf#") should return false.
-
-telephoneCheck("55555555") should return false.
-
-telephoneCheck("(6054756961)") should return false.
-
-telephoneCheck("2 (757) 622-7382") should return false.
-
-telephoneCheck("0 (757) 622-7382") should return false.
-
-telephoneCheck("-1 (757) 622-7382") should return false.
-
-telephoneCheck("2 757 622-7382") should return false.
-
-telephoneCheck("10 (757) 622-7382") should return false.
-
-telephoneCheck("27576227382") should return false.
-
-telephoneCheck("(275)76227382") should return false.
-
-telephoneCheck("2(757)6227382") should return false.
-
-telephoneCheck("2(757)622-7382") should return false.
-
-telephoneCheck("555)-555-5555") should return false.
-
-telephoneCheck("(555-555-5555") should return false.
-
-telephoneCheck("(555)5(55?)-5555") should return false.
-
-telephoneCheck("55 55-55-555-5") should return false.
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 */
