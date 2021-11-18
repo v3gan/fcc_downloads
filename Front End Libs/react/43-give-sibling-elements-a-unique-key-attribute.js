06b720d8291ecb0c08a3ad02b3b37b6c13598465
marcus <marcmonk@gmail.com>
2021-11-17 21:55:53 -0500

    finsh react on on to redux

---
+++ Front End Libs/react/43-give-sibling-elements-a-unique-key-attribute.js
@@ -0,0 +1,20 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((f,i) => <li key={i}>{f}</li>);
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
